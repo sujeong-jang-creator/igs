634072b4bbea27c38386dde0deb8fb78770f7998
bankze34 <bankze34@gmail.com>
2021-09-29 20:34:49 +0900

    django pushed to github

--- accountapp/views.py
+++ accountapp/views.py
@@ -30,3 +30,7 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+
+# class AccountDetailView(DetailView):
+#     model = User
+#     template_name = 'account/detail.html'
