45214fbb092e8c28880c7e55ac4f65756c01e291
bankze34 <bankze34@gmail.com>
2021-10-09 13:29:35 +0900

    Revert "password reset func added"
    This reverts commit a35835eb

--- upload/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from upload.views import PickPhotoView
-
-app_name = "upload"
-
-urlpatterns = [
-    path('pick_photo/', PickPhotoView.as_view(), name='pick_photo'),
-]
