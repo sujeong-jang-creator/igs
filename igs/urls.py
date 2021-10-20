69b18ba46c319bb8a589ba08632366ea3a79d283
hyj <hyj1004mu@naver.com>
2021-10-20 15:27:07 +0900

    쓸모없는 주석 제거

--- igs/urls.py
+++ igs/urls.py
@@ -32,34 +32,12 @@ urlpatterns = [
     path('grading/', include('grading.urls')),
     path('result/', include('result.urls')),
 
-    # path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(
-    #     template_name='password_reset/password_change_done.html'),
-    #     name='password_change_done'),
-    # path('password_change/', auth_views.PasswordChangeView.as_view(
-    #     template_name='password_reset/password_change.html'),
-    #     name='password_change'),
-
-    # Django 디폴트 제공 뷰
-    # path('password_reset/', auth_views.PasswordResetView.as_view(
-    #     template_name='password_reset/password_reset.html'),
-    #     name='password_reset'),
-    # 사용자 뷰
     path('password_reset/', UserPasswordResetView.as_view(),
         name='password_reset'),
 
-    # Django 디폴트 제공 뷰
-    # path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(
-    #     template_name='password_reset/password_reset_done.html'),
-    #     name='password_reset_done'),
-    # 사용자 뷰
     path('password_reset/done/', UserPasswordResetDoneView.as_view(),
         name='password_reset_done'),
 
-    # Django 디폴트 제공 뷰
-    # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-    #     template_name='password_reset/password_reset_confirm.html'),
-    #     name='password_reset_confirm'),
-    # 사용자 뷰
     path('reset/<uidb64>/<token>/', UserPasswordResetConfirmView.as_view(
         template_name='password_reset/password_reset_confirm.html'),
         name='password_reset_confirm'),
