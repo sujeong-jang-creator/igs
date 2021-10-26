f78eea6b007a3b28ec9d0e37df8725b257d04edb
YJH-jm <jmjhjob13@gmail.com>
2021-10-26 17:08:23 +0900

    업로드 된 고기 등급 사진 평가 하는 upload 앱 업데이트

--- result/views.py
+++ result/views.py
@@ -3,24 +3,25 @@ from upload.models import Results
 from django.db.models import Q
 from django.core.serializers.json import DjangoJSONEncoder
 
-def show_result(request, pk):
-    data = Results.objects.get(result_id=pk)
+def show_result(request, result_pk):
+    print("넘어온 pk : ", result_pk)
+    data = Results.objects.get(pk=result_pk)
     if request.method == "GET":
         return render(request, 'result/show_result.html', {'result' : data})
     else:
         if True:
             return redirect('/')
 
-def revision(request, pk):
+def revision(request, result_pk):
     if request.method == "GET":
-        data = Results.objects.get(result_id=pk)
+        data = Results.objects.get(pk=result_pk)
         return render(request, 'result/revision.html', {'result' : data})
     else:
         choice_grade = request.POST.get('grade')
         result_id = request.POST.get('result_id')
         
         if True:
-            update = Results.objects.get(result_id=pk)
+            update = Results.objects.get(pk = result_pk)
             update.modified_grade=choice_grade
             update.save()
             return redirect('/')
--- upload/admin.py
+++ upload/admin.py
@@ -1,14 +1,4 @@
 from django.contrib import admin
-
 from .models import Results
 
 admin.site.register(Results)
-# from .forms import PickPhotoForm
-# from .models import PickPhoto
-
-
-# class ImageAdmin(admin.ModelAdmin):
-#     form = PickPhotoForm
-
-
-# admin.site.register(PickPhoto, ImageAdmin)
