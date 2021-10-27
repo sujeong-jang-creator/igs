35b819feb478e68b090a451ab08595b8d3d2c20a
jcry <gomdorij@naver.com>
2021-10-27 17:53:01 +0900

    show_detail page 제작 및 연결 (테이블 클릭시 detail 페이지)

--- grading/urls.py
+++ grading/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
 from django.urls import path
+
+import upload
+from upload import views
 from . import views
 
 app_name= "grading"
@@ -7,4 +10,5 @@ app_name= "grading"
 urlpatterns = [
     path('total_distinguish/', views.PostListView.as_view(), name='total_distinguish'),
     path('detail_distinguish/', views.PostListView2.as_view(), name='detail_distinguish'),
+    path('show_detail/<int:result_pk>', views.show_detail, name='show_detail'),
 ]
