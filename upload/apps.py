1d90114c02399a83264149b0510ca5fa5776b285
YJH-jm <jmjhjob13@gmail.com>
2021-11-06 17:21:12 +0900

    모델 경로 수정

--- upload/apps.py
+++ upload/apps.py
@@ -3,4 +3,5 @@ from tensorflow.keras import models
 
 class UploadConfig(AppConfig):
     name = 'upload'
-    model = models.load_model(r".\model\YOLOv4")
+    model = models.load_model(r"\engn\igs_venv\igs\model\YOLOv4")
+    # model = models.load_model(r".\model\YOLOv4")
